ac9110ef735d81786999e075bd64e87272c26b42
Ruslan Shevtsov <r.shevtsov@aericon.com>
2015-04-07 11:37:30 +0300

    + Add base view controllers

--- FiltersRecordSample/FRSAppDelegate.swift
+++ FiltersRecordSample/FRSAppDelegate.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class FRSAppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
---
+++ FiltersRecordSample/UI layer/ViewControllers/BaseViewController/FRSBaseViewController.swift
@@ -0,0 +1,35 @@
+//
+//  FRSBaseViewController.swift
+//  FiltersRecordSample
+//
+//  Created by Ruslan Shevtsov on 4/7/15.
+//  Copyright (c) 2015 Work. All rights reserved.
+//
+
+import UIKit
+
+class FRSBaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
